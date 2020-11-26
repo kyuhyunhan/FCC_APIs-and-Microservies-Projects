ab273fe9aad02ba648080e70701b35cb9b0dbdd2
kyuhyun <kyuhyunhaan@gmail.com>
2020-11-26 11:55:40 +0900

    parsed the header except retrieving ipaddress

--- request-header-parser-microservice/server.js
+++ request-header-parser-microservice/server.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-const requestIp = require('request-ip');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -13,13 +12,6 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
-// using request-ip MW
-const ipMW = (req, res, next) => {
-  console.log('res.locals object:',res.locals,'//////////////////////////////');
-  res.locals.clientIp = requestIp.getClientIp(req); // to attach value to "res.locals" Object
-  next();
-}
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -32,17 +24,6 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.route('/api/whoami').get(ipMW, (req, res) => {
-  // const ipAddress = res.locals.clientIp;
-  const ipAddress = (req.headers['x-forwarded-for'] ||
-                      req.connection.remoteAddress ||
-                      req.socket.remoteAddress ||
-                      req.connection.socket.remoteAddress).split(",")[0];
-  const language = req.headers['accept-language'];
-  const software = req.headers['user-agent'];
-  console.log(ipAddress)
-  // res.json({ ipaddress: blank, language: blank, software: blank })
-})
 
 
 // listen for requests :)
