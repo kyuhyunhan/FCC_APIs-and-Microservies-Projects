0e3ab838177383c7244b3cef7d899872534ccf19
kyuhyun <kyuhyunhaan@gmail.com>
2020-11-30 23:16:36 +0900

    installed esm module in order to use ES Modules

--- URL-shortener-microservice/models/url.js
+++ URL-shortener-microservice/models/url.js
@@ -1,10 +1,13 @@
 'use strict';
+import { nanoid } from 'nanoid';
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// defining Schema
 const urlSchema = Schema({
     url: String,
-    short: {type: String, default: shortid.generate}
+    short: {type: String, default: nanoid(5)}
 })
 
+// converting schema into a Model in order to use it!
 module.exports = mongoose.model('Url',urlSchema);
--- URL-shortener-microservice/server.js
+++ URL-shortener-microservice/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+import { nanoid } from 'nanoid';
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -29,8 +30,28 @@ app.get('/', function(req, res) {
 app.route('/api/shorturl/new').post((req,res)=>{
   const local = req.headers.host;   // log 'localhost:3000'
   const inputUrl = req.body.url;
+
   if(validUrl.isUri(inputUrl)){   // <?> it doesn't work in right way when omitting http:// or https://
     console.log('Yes!')
+    Url.find({url:inputUrl}, function(err, data) {
+      if(err){
+        console.err(err);
+      } else {
+        if (data.length == 0) {
+          
+          const urlObj = {url: inputUrl, short: nanoid(5)};
+          Url.create(urlObj, function(err, newUrl) {
+            if(err){
+              console.err(err);
+            } else {
+              const originalUrl = newUrl.url;
+              const shortUrl = newUrl.short;
+              res.json({original_url: originalUrl, short_url: shortUrl});
+            }
+          })
+        }
+      }
+    })
   } else {
     console.log('Sorry, it seems a wrong url format. Please enter valid url')
   }
