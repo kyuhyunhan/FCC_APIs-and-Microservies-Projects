e48553b2b4483eb30c725cb141fd38daf21286e8
kyuhyun <kyuhyunhaan@gmail.com>
2020-12-02 22:53:53 +0900

    just deleted comments

--- URL-shortener-microservice/server.js
+++ URL-shortener-microservice/server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-import { json } from 'body-parser';
 import { nanoid } from 'nanoid';
 const express = require('express');
 const cors = require('cors');
@@ -26,21 +25,14 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
-// save the entered url in request body
-// assign a random url(/api/shorturl/randomNumber) to the original url.
-// when enter '../api/shorturl/randomNumber' in address bar, link to the original url.
-
-// in other words, I need an API which create linking url automatically.
-// And the API fetches data from somewhere has 'original url' and 'randomNumber' assigned.
-
 app.route('/api/shorturl/new').post((req,res)=>{
-  const local = req.headers.host;   // log 'localhost:3000'
   const inputUrl = req.body.url;
 
   if(validUrl.isUri(inputUrl)){   // <?> it doesn't work in right way when omitting http:// or https://
